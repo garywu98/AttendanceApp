8a13aceb5522b6afe6b4b579ecc75b5c7ce93922
LauraVillarr <lauravillarr1@gmail.com>
2023-03-01 13:23:44 -0600

    created UI for bluetooth buttons, created UI for bluetooth status messages

--- app/src/main/java/com/example/attendanceapp/MainActivity.java
+++ app/src/main/java/com/example/attendanceapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.attendanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initBluetooth();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void initBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null) {
+            TextView message = (TextView) findViewById(R.id.message);
+            // Test this later
+            message.setText(R.string.bluetooth_not_supported);
+        }
+        else if(!bluetoothAdapter.isEnabled()) {
+            TextView message = (TextView) findViewById(R.id.message);
+            message.setText(R.string.bluetooth_enable_message);
+        }
+    }
+    private void isBluetoothEnabled() {
+        if(bluetoothAdapter.isEnabled()) {
+//            TextView message = (TextView) findViewById(R.id.message);
+//            message.setText(R.string.bluetooth_enable_message);
+        }
     }
 }
